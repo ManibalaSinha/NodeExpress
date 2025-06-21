dbbe1da6dc190ac667804579cd934c74f115e227
Manibala Sinha <manibalak@gmail.com>
2025-06-21 17:12:35 -0400

    Update index.js
    About pages
    json pages api/users

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello from Render!');
 });
+app.get('/about', (req, res) => {
+  res.send('About Page - Node Express App');
+});
+app.get('/api/users', (req, res) => {
+  res.json([{ id: 1, name: "Manibala" }]);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
