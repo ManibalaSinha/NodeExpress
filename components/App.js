a9e62c21c97bfdcdd55dc89222126a4a3dd86ff6
Manibala Sinha <manibalak@gmail.com>
2025-06-21 18:40:37 -0400

    Create App.js
    added APP() function

---
+++ components/App.js
@@ -0,0 +1,19 @@
+// App.js
+import React, { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/users`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error("Error fetching users:", err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello from React</h1>
+    </div>
+  );
+}
+
+export default App;
